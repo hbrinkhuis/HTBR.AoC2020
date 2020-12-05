3b7552410e2daa0551798102b622f2a72411c911
htbr <henri@htbr.nl>
2020-12-05 22:32:05 +0100

    Day 1 + day 2

---
+++ day1.py
@@ -0,0 +1,37 @@
+path = "day1.txt"
+data_file = open(path, 'r')
+
+values = data_file.readlines()
+
+# convert to int
+intvalues = sorted(list(map(lambda x: int(x), values)))
+
+print('calculating part 1...')
+found = False
+for x in intvalues:
+    for y in intvalues[::-1]:
+        if x + y == 2020:
+            print('found pair!', x, y)
+            print('answer is:', x * y)
+            found = True
+            break
+        if x + y < 2020:
+            break
+    if found:
+        break
+
+print('calculating part 2...')
+found = False
+for i, x in enumerate(intvalues):
+    for y in intvalues[i::]:
+        for z in intvalues[::-1]:
+            if x + y + z == 2020:
+                print('found triplet!', x, y, z)
+                print('answer is:', x * y * z)
+                found = True
+            if z <= y:
+                break
+        if found:
+            break
+    if found:
+        break
---
+++ day2.py
@@ -0,0 +1,27 @@
+import re
+
+path = "day2.txt"
+data_file = open(path, 'r')
+
+values = data_file.readlines()
+
+num_valid_part_1 = 0
+num_valid_part_2 = 0
+for v in values:
+    match = re.search(r'(\d+)-(\d+) ([a-z]+): ([a-z]+)', v)
+    min_char = int(match[1])
+    max_char = int(match[2])
+    char = match[3]
+    passwd = match[4]
+    occurences = len(re.findall(char, passwd))
+    if occurences >= min_char and occurences <= max_char:
+        num_valid_part_1 += 1
+    
+    chrlist = list(passwd)
+    indices = [i + 1 for i, value in enumerate(chrlist) if value == char]
+    if (min_char in indices) ^ (max_char in indices):
+        num_valid_part_2 += 1
+
+
+print(num_valid_part_1, "are valid (part 1)")
+print(num_valid_part_2, "are valid (part 2)")
