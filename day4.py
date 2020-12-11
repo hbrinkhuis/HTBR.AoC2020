7e9fa8671b2bbf5013df854450d91296fdef1b2f
htbr <henri@htbr.nl>
2020-12-11 13:37:15 +0100

    Day 4 pt 2

--- day4.py
+++ day4.py
@@ -5,33 +5,59 @@ data_file = open(path, 'r')
 
 data = data_file.read().splitlines()
 
-valid_passprt = 0
+def from_to(x,fr,to):
+    return x.isdigit() and int(x) >= fr and int(x) <= to
 
-pass_fields = {
-    "byr": False,
-    "iyr": False,
-    "eyr": False,
-    "hgt": False,
-    "hcl": False,
-    "ecl": False,
-    "pid": False,
+def height_validation(hgt):
+    m = re.match(r'(\d{2,3})(cm|in)', hgt)
+    if(m):
+        size = m.group(1)
+        unit = m.group(2)
+        if(unit == "cm"):
+            return from_to(size, 150, 193)
+        if(unit == "in"):
+            return from_to(size, 59, 76)
+    return False
+
+pass_evals = {
+    "byr": lambda x: from_to(x, 1920, 2002),
+    "iyr": lambda x: from_to(x, 2010, 2020),
+    "eyr": lambda x: from_to(x, 2020, 2030),
+    "hgt": lambda x: height_validation(x),
+    "hcl": lambda x: re.match(r'#[0-9a-f]{6}', x),
+    "ecl": lambda x: re.match(r'amb|blu|brn|gry|grn|hzl|oth', x),
+    "pid": lambda x: bool(re.match(r'^\d{9}$', x))
 }
 
-for i, line in enumerate(data):
-    match = re.findall(r'([a-z]{3}):.+?', line)
-    if(len(match) == 0):
-        for k, v in pass_fields.items():
-            pass_fields[k] = False
-    else:
-        for m in match:
-            if(m != "cid"):
-                pass_fields[m] = True
-        
-        if(all(x for x in pass_fields.values())):
-            valid_passprt += 1
-            for k, v in pass_fields.items():
-                pass_fields[k] = False
-    print(match)
-    print(pass_fields)
+def find_valids(validate_fields):
+    valid_passprt = 0
+    pass_fields = {
+        "byr": False,
+        "iyr": False,
+        "eyr": False,
+        "hgt": False,
+        "hcl": False,
+        "ecl": False,
+        "pid": False,
+    }
 
-print('valid pass:', valid_passprt)
+    for i, line in enumerate(data):
+        match = re.findall(r'([a-z]{3}):([\w#]+)', line)
+        if(len(match) == 0):
+            for k in pass_fields.keys():
+                pass_fields[k] = False
+        else:
+            for i, val in match:
+                if(i != "cid"):
+                    pass_fields[i] = pass_evals[i](val) if validate_fields else True
+            
+            if(all(x for x in pass_fields.values())):
+                valid_passprt += 1
+                for k in pass_fields.keys():
+                    pass_fields[k] = False
+        
+    return valid_passprt
+
+
+print('valid pass:', find_valids(False))
+print('valid pass pt 2:', find_valids(True))
