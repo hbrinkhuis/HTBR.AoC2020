463b76a4c59d4cd5775553641958ed8288b1a183
HTBR <henri@htbr.nl>
2021-02-01 23:08:26 +0100

    Update day8.py

--- day8.py
+++ day8.py
@@ -1,37 +1,40 @@
-path = "day8.txt"
-data_file = open(path, 'r')
-
 import re
 
-accu = 0
-dup_found = False
-i = 0
-
-data = data_file.read().splitlines()
-
-exec_table = []
+path = "day8.txt"
+data_file = open(path, 'r')
 
 def parse_statement(str):
     m = re.match(r'(nop|acc|jmp) ([\+\-]\d+)', str)
     return (m.group(1), m.group(2), 0)
 
-for statement in data:
-    exec_table.append(parse_statement(statement))
+run_cmd = {
+    'nop': lambda x, i, accu: (i + 1, accu),
+    'acc': lambda x, i, accu: (i + 1, accu + int(x)),
+    'jmp': lambda x, i, accu: (i + int(x), accu)
+}
 
-while dup_found == False:
+def calc_accumulator(i, exec_table, accumulator):
     statement = exec_table[i]
-    if(statement[2] > 0):
-        dup_found = True
-        break
+    cmd = statement[0]
+    val = statement[1]
 
+    res = run_cmd[cmd](val, i, accumulator)
     exec_table[i] = (statement[0], statement[1], statement[2] + 1)
-    comm = statement[0]
-    if comm == 'nop':
-        i = i + 1
-    if comm == 'acc':
-        accu = accu + int(statement[1])
-        i = i + 1
-    if comm == 'jmp':
-        i = i + int(statement[1])
 
-print(accu)
+    # if next command is already executed, return
+    if(exec_table[res[0]][2] > 0):
+        return (res[1], True)
+
+    # if next command is EOF, return
+    if(len(exec_table) == res[0]):
+        return (res[1], False)
+
+    return calc_accumulator(res[0], exec_table, res[1])
+
+data = data_file.read().splitlines()
+
+exec_table = [parse_statement(statement) for statement in data]
+
+print('part 1:', calc_accumulator(0, exec_table, 0)[0])
+
+# part 2: bruteforce top to bottom jpm/nop changes
